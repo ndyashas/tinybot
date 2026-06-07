80eb00774d43dc0b1a8f48876eec048e5b58076e
Yashas Nagavane Dattatreya <ndyashas@gmail.com>
2026-06-07 14:11:49 -0700

    Homework #4 -- many links -- 25 towers

--- generate.py
+++ generate.py
@@ -1,7 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 2, 3])
+height = 1
+width = 1
+length = 1
+
+for z in range(10):
+    for x in range(5):
+        for y in range(5):
+            pyrosim.Send_Cube( name=f"Box{x}{y}{z}", pos=[x, y, 0.5 + z], size=[width, length, height])
+
+    # Update height, width, and length
+    height *= 0.9
+    width  *= 0.9
+    length *= 0.9
 
 pyrosim.End()

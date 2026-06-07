4eab8b89c6aaeb0eb521edec24dd276f12e19492
Yashas Nagavane Dattatreya <ndyashas@gmail.com>
2026-06-06 20:14:45 -0700

    Homework #2 -- one link

--- simulation.py
+++ simulation.py
@@ -4,6 +4,8 @@ import pybullet as p
 physics_client = p.connect(p.GUI)
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+p.loadSDF("box.sdf")
+
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)

a83ab9b414934c052c68049dbd531df919a4f175
Yashas Nagavane Dattatreya <ndyashas@gmail.com>
2026-06-06 19:47:00 -0700

    Homework #1
    Add sleep to keep the simulation window from closing too quickly.
    Can move around the camera using mouse inputs.

--- simulation.py
+++ simulation.py
@@ -1,5 +1,12 @@
+import time
 import pybullet as p
 
 physics_client = p.connect(p.GUI)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
 
 p.disconnect()

24c70d2c98759ab5ecb4122e0cac7db183c0ed6e
Yashas Nagavane Dattatreya <ndyashas@gmail.com>
2026-06-06 23:34:09 -0700

    Homework #3 -- block jumping to resolve illegal position

--- generate.py
+++ generate.py
@@ -2,6 +2,6 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 2, 3])
 
 pyrosim.End()
--- simulation.py
+++ simulation.py
@@ -1,8 +1,13 @@
 import time
 import pybullet as p
+import pybullet_data
 
 physics_client = p.connect(p.GUI)
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0, 0, -9.8)
+plane_id = p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
 
